05d675fbf796cd4ca238271022e1dfa503f24c5d
Ben Thomas <bthomas@whitestar.io>
2022-03-24 12:03:17 -0400

    contact form fixed?

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,214 +1,248 @@
 import {
-    Container,
-    Flex,
-    Box,
-    Heading,
-    Text,
-    IconButton,
-    Button,
-    VStack,
-    HStack,
-    Wrap,
-    WrapItem,
-    FormControl,
-    FormLabel,
-    Input,
-    InputGroup,
-    InputLeftElement,
-    Textarea,
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalCloseButton,
-    ModalBody,
-  } from '@chakra-ui/react';
-  import {
-    MdPhone,
-    MdEmail,
-    MdLocationOn,
-    MdFacebook,
-    MdOutlineEmail,
-  } from 'react-icons/md';
-  import React from "react";
-  import { BsGithub, BsDiscord, BsPerson } from 'react-icons/bs';
-  
-  export default class ContactForm extends React.Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            open: false
-        }
+  Box,
+  Heading,
+  Text,
+  IconButton,
+  Button,
+  VStack,
+  HStack,
+  Wrap,
+  WrapItem,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Textarea,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalCloseButton,
+  ModalBody,
+} from '@chakra-ui/react';
+import {
+  MdPhone,
+  MdEmail,
+  MdLocationOn,
+  MdOutlineEmail,
+} from 'react-icons/md';
+import React from "react";
+import { BsPerson, BsTwitter, BsLinkedin, BsFacebook } from 'react-icons/bs';
+import { navigate } from 'gatsby-link';
 
-      this.onStateChange = props.onStateChange;
-    }
-  
-    toggleShow = () => {
-        this.setState({
-            open: !this.state.open
-        })
-    }
+function encode(data) {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+}  
 
-    componentDidMount() {
-        this.toggleShow = this.toggleShow.bind(this);
+export default class ContactForm extends React.Component {
+  constructor (props) {
+      super(props);
+      this.state = {
+          open: false
+      }
 
-        let isBrowser = () => typeof window !== "undefined";
-        if (isBrowser && window.location.hash == "#contactForm" && !this.state.open) {
-            this.setState({open: true});
-        }
-    }
+    this.onStateChange = props.onStateChange;
+  }
 
-    componentWillReceiveProps(props) {
-        this.setState({ open: props.open })
-    }
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-    render() {
-        return (
-          <>
-            <Modal
-              size={'xl'}
-              isCentered
-              onClose={this.toggleShow}
-              isOpen={this.state.open}
-              motionPreset='slideInBottom'
-            >
-              <ModalOverlay />
-              <ModalContent
-                maxW={{base: '450px', md: 'container.md'}}
-                bg="#02054B"
-                color="white"
-                borderRadius="lg">
-                <ModalCloseButton mt={{base: 10, sm: 2}} />
-                <ModalBody>
-                    <Wrap align={'center'} justify={{base: 'center', md: 'space-between'}} spacing={{ base: 6, sm: 10, md: 18 }}>
-                <WrapItem align={'center'}>
-                  <Box>
-                  <Heading>Contact WhiteStar</Heading>
-                    <Text color="gray.500">
-                      Fill in the form or click one of the links below
-                    </Text>
-                    <Box py={{ base: 0, sm: 3, md: 5, lg: 7 }}>
-                      <VStack display={{base: 'none', md: 'flex'}} pl={0} spacing={3} alignItems="center">
-                        <Button isFullWidth={true}
-                          size="md"
-                          height="48px"
-                          width="220px"
-                          variant="ghost"
-                          color="#DCE2FF"
-                          _hover={{ border: '2px solid #1C6FEB' }}
-                          leftIcon={<MdPhone color="#1970F1" size="20px" />}>
-                          +1 (770)-265-0426
-                        </Button>
-                        <Button
-                          size="md"
-                          height="48px"
-                          width="220px"
-                          variant="ghost"
-                          color="#DCE2FF"
-                          _hover={{ border: '2px solid #1C6FEB' }}
-                          leftIcon={<MdEmail color="#1970F1" size="20px" />}>
-                          support@whitestar.io
-                        </Button>
-                        <Button
-                          size="md"
-                          height="48px"
-                          width="220px"
-                          variant="ghost"
-                          color="#DCE2FF"
-                          _hover={{ border: '2px solid #1C6FEB' }}
-                          leftIcon={<MdLocationOn color="#1970F1" size="20px" />}>
-                          Durham, NC, USA
-                        </Button>
-                      </VStack>
-                    </Box>
-                    <HStack
-                      justify={'space-evenly'}
-                      mt={{ lg: 10, md: 10 }}
-                      spacing={{base: 14, md: 5}}
-                      px={5}
-                      alignItems="center">
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...this.state,
+      }),
+    })
+      .then(() => navigate("#"))
+      .catch((error) => alert(error));
+  };
+
+  toggleShow = () => {
+      this.setState({
+          open: !this.state.open
+      })
+  }
+
+  componentDidMount() {
+      this.toggleShow = this.toggleShow.bind(this);
+
+      let isBrowser = () => typeof window !== "undefined";
+      if (isBrowser && window.location.hash == "#contactForm" && !this.state.open) {
+          this.setState({open: true});
+      }
+  }
+
+  componentWillReceiveProps(props) {
+      this.setState({ open: props.open })
+  }
+
+  render() {
+      return (
+        <>
+          <Modal
+            size={'xl'}
+            isCentered
+            onClose={this.toggleShow}
+            isOpen={this.state.open}
+            motionPreset='slideInBottom'
+          >
+            <ModalOverlay />
+            <ModalContent
+              maxW={{base: '450px', md: 'container.md'}}
+              bg="#02054B"
+              color="white"
+              borderRadius="lg">
+              <ModalCloseButton mt={{base: 10, sm: 2}} />
+              <ModalBody>
+                  <Wrap align={'center'} justify={{base: 'center', md: 'space-between'}} spacing={{ base: 6, sm: 10, md: 18 }}>
+              <WrapItem align={'center'}>
+                <Box>
+                <Heading>Contact WhiteStar</Heading>
+                  <Text color="gray.500">
+                    Fill in the form or click one of the links below
+                  </Text>
+                  <Box py={{ base: 0, sm: 3, md: 5, lg: 7 }}>
+                    <VStack display={{base: 'none', md: 'flex'}} pl={0} spacing={3} alignItems="center">
+                      <Button isFullWidth={true}
+                        size="md"
+                        height="48px"
+                        width="220px"
+                        variant="ghost"
+                        color="#DCE2FF"
+                        _hover={{ border: '2px solid #1C6FEB' }}
+                        leftIcon={<MdPhone color="#1970F1" size="20px" />}>
+                        +1 (770)-265-0426
+                      </Button>
+                      <Button
+                        size="md"
+                        height="48px"
+                        width="220px"
+                        variant="ghost"
+                        color="#DCE2FF"
+                        _hover={{ border: '2px solid #1C6FEB' }}
+                        leftIcon={<MdEmail color="#1970F1" size="20px" />}>
+                        support@whitestar.io
+                      </Button>
+                      <Button
+                        size="md"
+                        height="48px"
+                        width="220px"
+                        variant="ghost"
+                        color="#DCE2FF"
+                        _hover={{ border: '2px solid #1C6FEB' }}
+                        leftIcon={<MdLocationOn color="#1970F1" size="20px" />}>
+                        Durham, NC, USA
+                      </Button>
+                    </VStack>
+                  </Box>
+                  <HStack
+                    justify={'space-evenly'}
+                    mt={{ lg: 10, md: 10 }}
+                    spacing={{base: 14, md: 5}}
+                    px={5}
+                    alignItems="center">
+                    <a href={'https://www.facebook.com/WhiteStarCommunications/'}>
                       <IconButton
                         aria-label="facebook"
                         variant="ghost"
                         size="lg"
                         isRound={true}
                         _hover={{ bg: '#0D74FF' }}
-                        icon={<MdFacebook size="28px" />}
+                        icon={<BsFacebook size="28px" />}
                       />
+                    </a>
+
+                    <a href={'https://www.linkedin.com/company/whitestar-communications-inc/'}>
                       <IconButton
-                        aria-label="github"
+                        aria-label="linkedin"
                         variant="ghost"
                         size="lg"
                         isRound={true}
                         _hover={{ bg: '#0D74FF' }}
-                        icon={<BsGithub size="28px" />}
+                        icon={<BsLinkedin size="28px" />}
                       />
+                    </a>
+
+                    <a href={'https://twitter.com/SOCIETYAppMedia'}>
                       <IconButton
-                        aria-label="discord"
+                        aria-label="twitter"
                         variant="ghost"
                         size="lg"
                         isRound={true}
                         _hover={{ bg: '#0D74FF' }}
-                        icon={<BsDiscord size="28px" />}
+                        icon={<BsTwitter size="28px" />}
                       />
-                    </HStack>
-                  </Box>
-                </WrapItem>
-                <WrapItem justifyContent={'center'} w={{base:'400px', md:'360px'}}>
-                  <Box align={'end'} bg="white" borderRadius="lg">
-                    <Box m={8} color="#0B0E3F">
-                    <form name="contactForm" method="post" data-netlify="true" data-netlify-recaptcha="true">
-                    <input type="hidden" name="form-name" value="contactForm" />
-                      <VStack spacing={5}>
-                        <FormControl id="name">
-                          <FormLabel>Your Name</FormLabel>
-                          <InputGroup borderColor="#E0E1E7">
-                            <InputLeftElement
-                              pointerEvents="none"
-                              children={<BsPerson color="gray.800" />}
-                            />
-                            <Input type="text" size="md" />
-                          </InputGroup>
-                        </FormControl>
-                        <FormControl id="email">
-                          <FormLabel>Email</FormLabel>
-                          <InputGroup borderColor="#E0E1E7">
-                            <InputLeftElement
-                              pointerEvents="none"
-                              children={<MdOutlineEmail color="gray.800" />}
-                            />
-                            <Input type="text" size="md" />
-                          </InputGroup>
-                        </FormControl>
-                        <FormControl id="message">
-                          <FormLabel>Message</FormLabel>
-                          <Textarea
-                            borderColor="gray.300"
-                            _hover={{
-                              borderRadius: 'gray.300',
-                            }}
-                            placeholder="message"
+                    </a>
+                  </HStack>
+                </Box>
+              </WrapItem>
+              <WrapItem justifyContent={'center'} w={{base:'400px', md:'360px'}}>
+                <Box align={'end'} bg="white" borderRadius="lg">
+                  <Box m={8} color="#0B0E3F">
+                  <form name="contactForm" method="post" data-netlify="true" onSubmit={this.handleSubmit}>
+                  <input type="hidden" name="form-name" value="contactForm" data-netlify-recaptcha="true"/>
+                    <VStack spacing={5}>
+                      <FormControl id="name">
+                        <FormLabel>Your Name</FormLabel>
+                        <InputGroup borderColor="#E0E1E7">
+                          <InputLeftElement
+                            pointerEvents="none"
+                            children={<BsPerson color="gray.800" />}
                           />
-                        </FormControl>
-                        <FormControl id="name">
-                          <Button
-                            type="submit"
-                            variant="solid"
-                            bg="primary.500"
-                            color="white"
-                            _hover={{}}>
-                            Send Message
-                          </Button>
-                        </FormControl>
-                      </VStack>
-                      </form>
-                    </Box>
+                          <Input name="name" onChange={this.handleChange} required={true} type="text" size="md" />
+                        </InputGroup>
+                      </FormControl>
+                      <FormControl id="email">
+                        <FormLabel>Email</FormLabel>
+                        <InputGroup borderColor="#E0E1E7">
+                          <InputLeftElement
+                            pointerEvents="none"
+                            children={<MdOutlineEmail color="gray.800" />}
+                          />
+                          <Input name="email" onChange={this.handleChange} required={true} type="email" size="md" />
+                        </InputGroup>
+                      </FormControl>
+                      <FormControl id="message">
+                        <FormLabel>Message</FormLabel>
+                        <Textarea
+                          name="message"
+                          required={true}
+                          onChange={this.handleChange}
+                          borderColor="gray.300"
+                          _hover={{
+                            borderRadius: 'gray.300',
+                          }}
+                          placeholder="message"
+                        />
+                      </FormControl>
+                      <FormControl id="name">
+                        <Button
+                          type="submit"
+                          variant="solid"
+                          bg="primary.500"
+                          color="white"
+                          _hover={{}}>
+                          Send Message
+                        </Button>
+                      </FormControl>
+                    </VStack>
+                    </form>
                   </Box>
-                </WrapItem>
-              </Wrap>
-                </ModalBody>
-              </ModalContent>
-            </Modal>
-          </>
-        )
-      }
-  }
+                </Box>
+              </WrapItem>
+            </Wrap>
+              </ModalBody>
+            </ModalContent>
+          </Modal>
+        </>
+      )
+    }
+}
